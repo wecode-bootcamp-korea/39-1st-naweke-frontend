5b362118b4c4070b1421f6fb0f148856dbcf4b2e
ji <vyoungv7340naver.com>
2022-11-19 22:57:55 +0900

    리뷰 반영 및 state 합치기, 중복선택=>단일선택 변경

--- src/pages/Main/Filter/Filter.js
+++ src/pages/Main/Filter/Filter.js
@@ -4,20 +4,21 @@ import SIZE_LIST from './sizeData';
 import './Filter.scss';
 
 const Filter = () => {
-  const [priceValue, setPriceValue] = useState(50000);
-  const handlePrice = e => {
-    setPriceValue(e.target.value);
+  const [selectValue, setSelectValue] = useState({
+    size: '',
+    color: '',
+    price: 50000,
+  });
+  const handleSelect = e => {
+    const { name, value } = e.target;
+    setSelectValue(prev => ({ ...prev, [name]: value }));
   };
 
   const [filterData, setFilterData] = useState([]);
   const filtering = url => {
-    fetch(`http://10.58.52.193:3000/products/all?${url}`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json;charset=utf-8' },
-    })
+    fetch(`http://10.58.52.193:3000/products/all?${url}`)
       .then(response => response.json())
       .then(res => setFilterData(res));
-    console.log(filtering);
   };
 
   return (
@@ -26,77 +27,102 @@ const Filter = () => {
         {/* gender */}
         <div className="productGender filterLayout">
           <h3>성별</h3>
-          <div className="checkBoxList">
-            <input type="checkbox" id="checkWomen" className="checkBoxInput" />
-            <label for="checkWomen" onChange={filtering('gender=2')} />
-            <span className="selectTitle">여성</span>
-          </div>
-          <div className="checkBoxList">
-            <input type="checkbox" id="checkMen" className="checkBoxInput" />
-            <label for="checkMen" onChange={filtering('gender=1')} />
-            <span className="selectTitle">남성</span>
-          </div>
+          <fieldset>
+            <div className="checkBoxList">
+              <input
+                type="radio"
+                id="checkWomen"
+                name="genderCheck"
+                className="checkBoxInput"
+              />
+              <label for="checkWomen" onChange={() => filtering('gender=2')} />
+              <span className="selectTitle">여성</span>
+            </div>
+            <div className="checkBoxList">
+              <input
+                type="radio"
+                id="checkMen"
+                name="genderCheck"
+                className="checkBoxInput"
+              />
+              <label for="checkMen" onChange={() => filtering('gender=1')} />
+              <span className="selectTitle">남성</span>
+            </div>
+          </fieldset>
         </div>
         {/* price */}
         <div className="productPrice filterLayout">
           <h3>가격</h3>
           <input
             type="range"
+            name="price"
             min="50000"
             max="250000"
             step={50000}
-            value={priceValue}
-            onChange={handlePrice}
+            value={selectValue.price}
+            onChange={handleSelect}
           />
           <div className="showPrice">
-            {priceValue < 200001
-              ? priceValue - 50000 + '~' + priceValue
-              : '200,000+'}
+            {selectValue.price < 200001
+              ? (selectValue.price - 50000).toLocaleString() +
+                '원 ~' +
+                Number(selectValue.price).toLocaleString() +
+                '원'
+              : '200,000원+'}
           </div>
         </div>
         {/* size */}
         <div className="productSize filterLayout">
           <h3>사이즈</h3>
           <div className="sizeBtnWrap">
-            {
-              // 데이터 카테고리 받기
-              // data.category === 'shoes' &&
-              SIZE_LIST.shoes.map((size, i) => {
-                return (
-                  <>
-                    <input
-                      type="radio"
-                      id={`size${i}`}
-                      key={i}
-                      className="radio"
-                    />
-                    <label
-                      htmlFor={`size${i}`}
-                      onChange={filtering(`size=${i}`)}
-                      className="label"
-                    >
-                      {size}
-                    </label>
-                  </>
-                );
-              })
-            }
+            {SIZE_LIST.shoes.map((size, i) => {
+              return (
+                <div key={i}>
+                  <input
+                    type="radio"
+                    name="size"
+                    id={`size${i}`}
+                    value={size}
+                    checked={Number(selectValue.size) === size}
+                    onChange={handleSelect}
+                    className="radio"
+                  />
+                  <label
+                    htmlFor={`size${i}`}
+                    onChange={() => filtering(`size=${i}`)}
+                    className="label"
+                  >
+                    <span>{size}</span>
+                  </label>
+                </div>
+              );
+            })}
           </div>
         </div>
         {/* color */}
         <div className="productColor filterLayout">
           <h3>색상</h3>
           <div className="colorBtnWrap">
-            {COLOR_LIST.map(({ id, color, text }) => {
+            {COLOR_LIST.map(({ id, text }, i) => {
               return (
                 <div className="colorLayout" key={id}>
-                  <span
+                  <input
+                    type="radio"
+                    id={`color${id}`}
+                    name="color"
+                    value={text}
+                    checked={selectValue.color === text}
+                    onChange={handleSelect}
                     className="showColor"
-                    style={{ backgroundColor: color }}
+                  />
+                  <label
+                    htmlFor={`color${id}`}
+                    className="label"
+                    onChange={() => filtering(`color=${i}`)}
                   >
-                    {text}
-                  </span>
-                  <span>{text}</span>
+                    <div className={`bgcolor ${text}`} />
+                    <span>{text}</span>
+                  </label>
                 </div>
               );
             })}
--- src/pages/Main/Filter/colorData.js
+++ src/pages/Main/Filter/colorData.js
@@ -1,11 +1,11 @@
 const COLOR_LIST = [
-  { id: 1, text: '화이트', color: 'white' },
-  { id: 2, text: '블랙', color: 'black' },
-  { id: 3, text: '그레이', color: 'grey' },
-  { id: 4, text: '그린', color: '#7bba3c' },
-  { id: 5, text: '옐로우', color: '#FED533' },
-  { id: 6, text: '레드', color: '#E7352B' },
-  { id: 7, text: '블루', color: '#1790C8' },
+  { id: 1, text: 'white' },
+  { id: 2, text: 'black' },
+  { id: 3, text: 'grey' },
+  { id: 4, text: 'green' },
+  { id: 5, text: 'yellow' },
+  { id: 6, text: 'red' },
+  { id: 7, text: 'blue' },
 ];
 
 export default COLOR_LIST;
