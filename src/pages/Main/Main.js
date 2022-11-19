33c0d1e28b6518f059e447d81d7000bb80680500
ji <vyoungv7340naver.com>
2022-11-19 16:08:16 +0900

    main layout 구현중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,34 +1,50 @@
 import React from 'react';
-import Filter from './Filter/Filter';
+// import Filter from './Filter/Filter';
 import './Main.scss';
 
 function Main() {
   return (
     <div className="main">
       <div className="mainWrap">
-        <div className="mainSlogan">Feel The NAWEKE</div>
+        <div className="mainSlogan">
+          <img src="images/mainslogan.png" alt="mainslogan" />
+        </div>
         <div className="mainBg">
-          <div className="runBg">
-            <img
-              src="images/runningMain.png"
-              alt="running"
-              className="runningMain mainimg"
-            />
+          <div className="runningWrap mainCon">
+            <div className="runBg">
+              <img
+                src="images/runningMain.png"
+                alt="running"
+                className="runningMain mainimg"
+              />
+            </div>
+            <div className="runningText">더 빨리</div>
           </div>
-          <div className="runBg">
-            <img
-              src="images/soccer.png"
-              alt="soccer"
-              className="soccerMain mainimg"
-            />
+
+          <div className="soccerWrap mainCon">
+            <div className="soccerBg">
+              <img
+                src="images/soccer.png"
+                alt="soccer"
+                className="soccerMain mainimg"
+              />
+            </div>
+            <div className="soccerText">더 과감하게</div>
           </div>
-          <div className="runBg">
-            <img
-              src="images/basketballMain.png"
-              alt="basketball"
-              className="basketballMain mainimg"
-            />
+
+          <div className="basketballWrap mainCon">
+            <div className="basketballBg">
+              <img
+                src="images/basketballMain.png"
+                alt="basketball"
+                className="basketballMain mainimg"
+              />
+            </div>
+            <div className="basketballText">더 높게</div>
           </div>
+        </div>
+        <div className="mainMsg">
+          <img src="images/mainmsg.png" alt="mainmsg" />
         </div>
       </div>
     </div>
